d078019c926e5dacba0e3c6c8fe0c9c6441a2c90
Cyril <chandrucyril@gmail.com>
2022-11-24 14:34:55 +0530

    added router

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/login";
+
+const AppRouter = () => {
+  return (
+    <>
+      <Router>
+        <nav style={{ margin: 10 }}>
+          <Link to="/" style={{ padding: 5 }}>
+            Home
+          </Link>
+          <Link to="/about" style={{ padding: 5 }}>
+            About
+          </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default AppRouter;
